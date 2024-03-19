45c4ce1cc7404af6672a7aa52f49d51aefcd3ccf
Kevin <kevinborjadiaz@hotmail.com>
2024-03-19 00:58:07 -0700

    contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { CvComponent } from './cv/cv.component';
+import { ConocemeComponent } from './conoceme/conoceme.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    InicioComponent,
+    ProyectosComponent,
+    CvComponent,
+    ConocemeComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
