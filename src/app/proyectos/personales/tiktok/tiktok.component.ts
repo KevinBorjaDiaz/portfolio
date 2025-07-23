85ab5085526169551ea0c8c56b0ffa57258dbd6c
Kevin Borja <kevinborjadiaz@hotmail.com>
2025-07-22 22:04:41 -0700

    errors, clean code and refactor

--- src/app/proyectos/personales/tiktok/tiktok.component.ts
+++ src/app/proyectos/personales/tiktok/tiktok.component.ts
@@ -14,20 +14,13 @@ export class TiktokComponent {
   displayBtn: boolean | false;
   srcURL: SafeResourceUrl | null = null;
   strURL: string | null;
+  errorMessage: string | null = null;
 
   async onPasteHandler(event: ClipboardEvent): Promise<void> {
-    const clipboardText = event.clipboardData?.getData('text').trim() || '';
-    let text = clipboardText;
+    let text = event.clipboardData?.getData('text').trim() || '';
 
-    if (clipboardText.includes('vt.tiktok')) {
-      text = await this.CallApiForMobileURL(clipboardText);
-    }
-
-    if (!this.PrepareURL(text)) {
-      //! If gotten to this point go back to starting point
-      this.displayBtn = false;
-      this.strURL = '';
-    }
+    text = await this.CallApiForMobileURL(text);
+    this.validateText(text);
   }
 
   async onSubmitHandler(form: NgForm): Promise<void> {
@@ -36,45 +29,44 @@ export class TiktokComponent {
     const inputValue = value.tikitakURL;
     let text = inputValue;
 
-    if (inputValue.includes('vt.tiktok')) {
-      text = await this.CallApiForMobileURL(inputValue);
-    }
-
-    if (!this.PrepareURL(text)) {
-      //! If gotten to this point go back to starting point
-      this.displayBtn = false;
-      this.strURL = '';
-    }
+    text = await this.CallApiForMobileURL(inputValue);
+    this.validateText(text)
   }
 
-  PrepareURL = (text): boolean => {
-    let videoID = text.replace('https://www.', 'www.');
-
-    const indexOfUser = videoID.indexOf('/');
-    const indexOfType = videoID.indexOf('/', indexOfUser + 1);
-    const indexOfID = videoID.indexOf('/', indexOfType + 1);
-    const endofID =
-      videoID.indexOf('?') == -1 ? videoID.length : videoID.lastIndexOf('?');
-    if (
-      indexOfUser != -1 &&
-      indexOfType != -1 &&
-      indexOfID != -1 &&
-      endofID != -1
-    ) {
-      videoID = videoID.substring(indexOfID + 1, endofID);
-      const finalURL = `https://www.tiktok.com/embed/${videoID}`;
-
-      this.srcURL = this.sanitizer.bypassSecurityTrustResourceUrl(finalURL);
-      this.displayBtn = true;
-      this.strURL = finalURL;
-      return true;
+  validateText = (text : string) : void => {
+    if (!text) {
+        this.setViewVariables(  'TikTok URL not accessible or restricted', false, '' )
+        return;
     }
-    return false;
+
+    return this.PrepareURL(text);
+  }
+
+  setViewVariables = (errorMsg : string, displayBtn : boolean, strUrl: string) : void => {
+        this.errorMessage = errorMsg;
+        this.displayBtn = displayBtn;
+        this.strURL = strUrl;
+  }
+
+  PrepareURL = (text: string): void => {
+    const parsed = new URL(text);
+    const paths = parsed.pathname.split('/')
+    if (paths.length >= 4 && paths[1] && paths[2] && paths[3]) {
+      const videoID = paths[3];
+      const finalURL = `https://www.tiktok.com/embed/${videoID}`;
+      this.srcURL = this.sanitizer.bypassSecurityTrustResourceUrl(finalURL);
+      
+      this.setViewVariables( null, true, finalURL )
+      return;
+    }
+
+    //! If gotten to this point go back to starting point
+    this.setViewVariables( 'TikTok URL not accessible or restricted', false, '' )
+    return;
   };
 
-  async CallApiForMobileURL(text): Promise<string> {
+  async CallApiForMobileURL(text: string): Promise<string> {
     const providedURL = `https://www.tiktok.com/oembed?url=${text}`;
-    let finalURL = '';
 
     try {
       const response = await fetch(providedURL);
@@ -82,8 +74,9 @@ export class TiktokComponent {
         throw new Error(`Response status: ${response.status}`);
       }
 
-      const json = await response.json();
-      return (finalURL = `${json.author_url}/v/${json.embed_product_id}`);
+      const {author_url, embed_product_id } = await response.json();
+      return (`${author_url}/v/${embed_product_id}`);
+
     } catch (error) {
       console.error(error.message);
       return null;
