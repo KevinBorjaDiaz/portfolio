456ae369d367df451a5391a7ebb5af7d4768940d
Kevin Borja <kevinborjadiaz@hotmail.com>
2025-11-27 22:52:56 -0800

    cleanup and build

--- src/app/proyectos/personales/personales.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-personales',
-  templateUrl: './personales.component.html',
-  styleUrl: './personales.component.css',
-  standalone: false,
-})
-export class PersonalesComponent {}
